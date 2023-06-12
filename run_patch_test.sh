aaaf79448ac6c48facbe900e9eed2440b4c98a8d
sam1016yu <miaoyu1016@gmail.com>
2023-06-12 18:19:05 +0000

    add run script

---
+++ run_patch_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd $HOME/spark
+
+result_file=raw_result_after
+
+if [ -d $result_file ]; then
+    rm -f $result_file
+fi
+
+for rep in 1 2 3
+do
+    sbt "sql/testOnly *InsertSuite -- -z SPARK-29938" &> raw_output.log
+    grep -F '!!!' raw_output.log >> $result_file
+done
+
+rm raw_output.log
+mv $result_file $HOME
