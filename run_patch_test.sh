222fa6fed09ea2edf02f0e3e747805e446ed9813
sam1016yu <miaoyu1016@gmail.com>
2023-06-12 21:57:50 +0000

    init

---
+++ run_patch_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd $HOME/spark
+
+# compile spark
+sbt package
+
+result_file=raw_result_before
+
+if [ -d $result_file ]; then
+    rm -f $result_file
+fi
+
+for rep in 1 2 3
+do
+    bin/spark-shell --master 'local[16]' --driver-memory 8g < $HOME/spark-25947-reproducer.scala  > raw_output.log 2>&1 
+    grep -F '!!!' raw_output.log >> $result_file
+done
+
+# rm raw_output.log
+# mv $result_file $HOME
